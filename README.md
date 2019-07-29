c27a071ed1ba12014551e9b8a51a54441c12f643
Kklear <kingklear@gmail.com>
2019-07-29 10:17:18 +0200

    Initial commit. Got a poorly flying robot. Physics is fun!

---
+++ README.md
@@ -0,0 +1,5 @@
+# Jetbot Jumble
+
+- Just started. No gameplay, no nothing.
+
+- Version 0.0
---
+++ main/bots/jet.go
@@ -0,0 +1,101 @@
+components {
+  id: "flame"
+  component: "/main/bots/flame.particlefx"
+  position {
+    x: 0.0
+    y: -24.0
+    z: -1.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "co"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
+  "mass: 0.05\n"
+  "friction: 1.0\n"
+  "restitution: 0.5\n"
+  "group: \"bot\"\n"
+  "mask: \"wall\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: -14.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.38268343\n"
+  "      w: 0.9238795\n"
+  "    }\n"
+  "    index: 3\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 27.0645\n"
+  "  data: 13.5365\n"
+  "  data: 10.0\n"
+  "  data: 20.0\n"
+  "  data: 20.0\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.5\n"
+  "angular_damping: 0.9\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "jet"
+  type: "sprite"
+  data: "tile_set: \"/assets/bots/bot1.atlas\"\n"
+  "default_animation: \"jet\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
