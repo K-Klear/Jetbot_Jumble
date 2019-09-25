7adca2b106be2d6724dda9f6957ff9e22f45763f
Kklear <kingklear@gmail.com>
2019-09-25 14:36:14 +0200

    Oops, deleted most of my progress. Thank god for recycle bin.

--- main/bots/body.go
+++ main/bots/body.go
@@ -1,3 +1,18 @@
+components {
+  id: "controls"
+  component: "/main/bots/controls.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "body"
   type: "sprite"
@@ -19,21 +34,23 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "co"
+  id: "co_body"
   type: "collisionobject"
   data: "collision_shape: \"\"\n"
   "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
-  "mass: 4.0\n"
+  "mass: 5.0\n"
   "friction: 0.1\n"
   "restitution: 0.0\n"
   "group: \"bot\"\n"
   "mask: \"wall\"\n"
+  "mask: \"bot\"\n"
+  "mask: \"head\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
   "      x: 0.0\n"
-  "      y: 0.0\n"
+  "      y: -20.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -46,7 +63,56 @@ embedded_components {
   "    count: 3\n"
   "  }\n"
   "  data: 40.0\n"
-  "  data: 45.0\n"
+  "  data: 26.3395\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "co_head"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
+  "mass: 1.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.0\n"
+  "group: \"head\"\n"
+  "mask: \"wall\"\n"
+  "mask: \"bot\"\n"
+  "mask: \"head\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 28.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 23.222\n"
+  "  data: 17.9535\n"
   "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"
