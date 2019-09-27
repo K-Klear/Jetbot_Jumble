5e44b200da6cdfb9fa061b47005de99b57423deb
Kklear <kingklear@gmail.com>
2019-09-28 01:22:10 +0200

    Powerups under construction.

--- main/game_settings.lua
+++ main/game_settings.lua
@@ -1,16 +1,21 @@
 local M = {}
 
+-- These can be set up in the main menu
 M.max_score = 3
 M.powerups = false
 M.powerups_secret = false
 M.curses = false
 M.curses_secret = true
 M.curses_affect = "ENEMY"
-M.chain_type = "ROPE"
-M.chain_length = 18
+M.chain_type = "NONE"
+M.chain_length = 20
+M.tooltips = false
+M.gravity = 10
 
-
-M.tooltips = true
+-- These are hardcoded as this point
+M.pickup_time
+M.pickup_frequency_max = 8
+M.pickup_frequency_min = 5
 
 
 return M
